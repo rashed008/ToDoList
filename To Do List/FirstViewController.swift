714d4385cd5b9cb1b3cb05a3cfbf2fd07fd701ee
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-06-03 13:21:05 +0600

    second commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -2,32 +2,74 @@
 //  FirstViewController.swift
 //  To Do List
 //
-//  Created by RASHED on 6/3/17.
-//  Copyright © 2017 RASHED. All rights reserved.
+//  Created by Rob Percival on 17/06/2016.
+//  Copyright © 2016 Appfish. All rights reserved.
 //
 
 import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    @IBOutlet var table: UITableView!
     
-      return 1
+    var items: [String] = []
+
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+        
     }
     
-   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = "test"
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
         return cell
-    
+        
+        
     }
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+        }
+        
+    }
+  
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -2,37 +2,58 @@
 //  SecondViewController.swift
 //  To Do List
 //
-//  Created by RASHED on 6/3/17.
-//  Copyright © 2017 RASHED. All rights reserved.
+//  Created by Rob Percival on 17/06/2016.
+//  Copyright © 2016 Appfish. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    
     @IBOutlet var itemTextField: UITextField!
     
     
-    @IBAction func add(_ sender: Any) {
+    @IBAction func add(_ sender: AnyObject) {
         
         let itemsObject = UserDefaults.standard.object(forKey: "items")
         
-        var items:NSMutableArray
+        var items:[String]
         
-        if let tempItems = itemsObject as? NSMutableArray {
-           items = tempItems
+        if let tempItems = itemsObject as? [String] {
             
-            items.addObjects(from: [itemTextField.text!])
-        
-        }else{
-           items = [itemTextField.text!]
+            items = tempItems
+            
+            items.append(itemTextField.text!)
+            
+            print(items)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+            
         }
         
         UserDefaults.standard.set(items, forKey: "items")
         
+        itemTextField.text = ""
+        
     }
-     
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
